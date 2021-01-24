ea97b9d7bc75f31c08b4a2fe693137d8dd3580fe
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-01-24 01:20:35 -0300

    add commands of read tables

--- connection.php
+++ connection.php
@@ -1,6 +1,21 @@
 <?php
 
-/**
- * Segue abaixo um exemplo de conexão com o banco de dados
- */
-$conn = new \PDO("mysql:host=localhost;dbname=son_php_pdo","root","123");
+
+try{
+    /**
+     * Segue abaixo um exemplo de conexão com o banco de dados
+     */
+    $conn = new \PDO("mysql:host=localhost;dbname=son_php_pdo","root","123");
+
+    // Query
+    $query = "SELECT * FROM produtos";
+
+    // Lendo dados do banco de dados
+    foreach($conn->query($query) as $value):
+        echo $value['nome']."<br />";
+    endforeach;
+
+    print_r($return);
+}catch(\PDOException $e){
+    echo "Error: {$e->getMessage()}";
+}
