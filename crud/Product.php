d557ab1a9c7c47056250824f41a687814e4da0a3
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-03-04 23:07:59 -0300

    Classe de produto

---
+++ crud/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+class Product implements IProduct
+{
+    private $id;
+    private $nome;
+    private $descricao;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+        return $this;
+    }
+}
